8997adbaa35a58aa2a403c6af86059b2b3e1a65b
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-08-15 15:22:58 -0400

    trim the fat

--- main.go
+++ main.go
@@ -1,24 +1,9 @@
 package main
 
-import (
-	"sync"
-)
-
 func main() {
-	// get flags
 	args := ParseFlags()
 	NewFiles(args).
 		Decode().
 		Encode().
 		Write()
-}
-
-// Await ...
-func Await(files Files) {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-	}()
-	wg.Wait()
 }
